73d3998dca0a29a154ac05377ec28878fc6c68b5
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-29 01:44:35 +0545

    database is connected logic

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
+connectDB().then(() => {
+  app.listen(PORT, () => {
     console.log(`Server is listening to PORT : ${PORT}`);
-})
-
+  });
+});
